f2021809bebc1da90d1dfd27e59eeafc47649f58
Lataheanga Saipaia <latasaipaia98@gmail.com>
2026-07-14 12:07:15 +1300

    Changing the Mimoto URL to the correct one

--- inji-web/public/env.config.js
+++ inji-web/public/env.config.js
@@ -2,8 +2,8 @@ window._env_ = {
     DEFAULT_LANG: "en",
     DEFAULT_THEME: "",
     DEFAULT_FAVICON: "favicon.ico",
-    DEFAULT_TITLE: "Inji Wallet",
+    DEFAULT_TITLE: "Tonga Digital Wallet",
     DEFAULT_FONT_URL: "https://fonts.googleapis.com/css?family=Inter",
-    MIMOTO_URL: "http://localhost:8099/v1/mimoto",
+    MIMOTO_URL: "http://localhost:3004/v1/mimoto",
     IGNORED_ISSUER_IDS: "MockMdl" // Comma-separated list of issuer IDs to ignore (e.g., "MockMdl,StayProtected")
 };
